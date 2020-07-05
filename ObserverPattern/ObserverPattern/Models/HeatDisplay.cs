810e3b99d266311d9f3b31055b3bae5a2db71dbb
Roberto Febrer <febrer@gmail.com>
2020-07-05 15:47:07 +0200

    add heat display

---
+++ ObserverPattern/ObserverPattern/Models/HeatDisplay.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using ObserverPattern.interfaces;
+
+namespace ObserverPattern.Models
+{
+    public class HeatDisplay : IObserver, IDisplay
+    {
+        private ISubject weatherData;
+        private float temperature;
+        private float humidity;
+        private float pressure;
+        public HeatDisplay(ISubject weatherData)
+        {
+            this.weatherData = weatherData;
+            this.weatherData.RegisterObserver(this);
+            Console.WriteLine($"Heat Display Initiated");
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Heat Display Temp:{temperature} ");
+        }
+
+        public void Update(float temp, float humidity, float pressure)
+        {
+            this.temperature = temp;
+            this.humidity = humidity;
+            this.pressure = pressure;
+            Display();
+        }
+    }
+}
--- ObserverPattern/ObserverPattern/Program.cs
+++ ObserverPattern/ObserverPattern/Program.cs
@@ -13,9 +13,14 @@ namespace ObserverPattern
             ConditionsDisplay currentDisplay = new ConditionsDisplay(weatherData);
             StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
             ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+            HeatDisplay heatDisplay = new HeatDisplay(weatherData);
+
             weatherData.SetMeasurements(80, 65, 30.4f);
             weatherData.SetMeasurements(82, 70, 29.2f);
             weatherData.SetMeasurements(78, 90, 29.2f);
+            weatherData.RemoveObserver(heatDisplay);
+            weatherData.SetMeasurements(85, 98, 39.2f);
+
             Console.ReadLine();
         }
     }
