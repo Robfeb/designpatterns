da145079b89c105ce8577aa4c5727000e889a479
Roberto Febrer <febrer@gmail.com>
2020-07-17 21:48:50 +0200

    add iterator and composite pattern project

---
+++ Iterator_CompositePatterns/Iterator_CompositePatterns/Models/MenuItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Iterator_CompositePatterns.Models
+{
+    public class MenuItem
+    {
+        string name;
+        string description;
+        bool vegetarian;
+        double price;
+
+        public MenuItem(string name, string description, bool vegetarian, double price)
+        {
+            this.name = name;
+            this.description = description;
+            this.vegetarian = vegetarian; this.price = price;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+        public string GetDescription()
+        {
+            return description;
+        }
+        public double GetPrice()
+        {
+            return price;
+        }
+        public bool IsVegetarian()
+        {
+            return vegetarian;
+        }
+    }
+}
---
+++ Iterator_CompositePatterns/Iterator_CompositePatterns/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Iterator_CompositePatterns.Models;
+
+namespace Iterator_CompositePatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Iterator and Composite Pattern project!");
+            PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
+            List<MenuItem> breakfastItems = pancakeHouseMenu.GetMenuItems();
+
+            DinerMenu dinerMenu = new DinerMenu();
+            MenuItem[] lunchItems = dinerMenu.GetMenuItems();
+            Console.WriteLine("------------------Pancake House MENU ------------");
+
+            for (int i = 0; i < breakfastItems.Count; i++)
+            {
+                MenuItem menuItem = breakfastItems[i];
+                Console.WriteLine(menuItem.GetName() + " ");
+                Console.WriteLine(menuItem.GetPrice() + " ");
+                Console.WriteLine(menuItem.GetDescription());
+            }
+            Console.WriteLine("------------------DINER MENU ------------");
+            for (int i = 0; i < lunchItems.Length; i++)
+            {
+                MenuItem menuItem = lunchItems[i];
+                if (menuItem == null) continue;
+                Console.WriteLine(menuItem.GetName() + " ");
+                Console.WriteLine(menuItem.GetPrice() + " ");
+                Console.WriteLine(menuItem.GetDescription());
+            }
+
+        }
+    }
+}
